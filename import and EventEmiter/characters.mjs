32bda9f4f7d7566d10990aa4a16237f7a2ae5242
Gazzik <gazzik@gazzik.com>
2022-07-03 15:45:53 +0300

    first module

---
+++ import and EventEmiter/characters.mjs
@@ -0,0 +1,13 @@
+export const characters = [
+    'Frodo',
+    'Bilbo',
+    'Merlin'
+]
+
+export function greet(someName) {
+    console.log(`Congratulated ${someName}`);
+}
+
+export default function log() {
+    console.log('test default');
+}
