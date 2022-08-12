0c6e6ccebe143ca355471e5eb4ca547de0efd02f
Gazzik <gazzik@gazzik.com>
2022-08-12 10:14:52 +0300

    щось добавив щось поміняв))

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,25 @@
 import express, { Express } from "express";
+import { inject, injectable } from "inversify";
 import { Server } from "node:http";
 import { ExeptionFilter } from "./errors/exeption.filter";
-import { LoggerService } from "./logger/logger.service";
+import { ILogger } from "./logger/logger.interface";
+import { TYPES } from "./types";
 import { UserController } from "./users/users.controller";
+import 'reflect-metadata'
 
+@injectable()
 export class App {
   private app: Express;
   private port: number;
   private server: Server
-  private loggerService: LoggerService;
-	private userController: UserController;
-	private exeptionFilter: ExeptionFilter;
 
   constructor(
-		private logger: LoggerService,
-		 userController: UserController,
-		  exeptionFilter: ExeptionFilter
+    @inject(TYPES.ILogger) private loggerService: ILogger,
+    @inject(TYPES.UserController) private userController: UserController,
+    @inject(TYPES.ExeptionFilter) private exeptionFilter: ExeptionFilter,
 			) {
     this.app = express();
     this.port = 8000
-    this.loggerService = new LoggerService();
-		this.userController = userController;
-		this.exeptionFilter = exeptionFilter;
   }
 	
   public async init() {
--- src/errors/exeption.filter.ts
+++ src/errors/exeption.filter.ts
@@ -1,20 +1,22 @@
 import { NextFunction, Request, Response } from "express";
+import { inject, injectable } from "inversify";
+import { ILogger } from "../logger/logger.interface";
 import { LoggerService } from "../logger/logger.service";
+import { TYPES } from "../types";
 import { IExeptionFilter } from "./exeption.filter.interface";
 import { HTTPError } from "./http-error.class";
+import 'reflect-metadata'
 
+@injectable()
 export class ExeptionFilter implements IExeptionFilter {
-    loggerService: LoggerService;
-    constructor(private logerSevice: LoggerService) {
-        this.logerSevice = logerSevice
-    }
+    constructor(@inject(TYPES.ILogger) private logger: ILogger) { }
     catch(err: Error | HTTPError, req: Request, res: Response, next: NextFunction) {
         if (err instanceof HTTPError) {
-            this.logerSevice.error(`[${err.contex}] ${err.message}`);            
-            res.status(err.statusCode).send({err: err.message})      
+            this.logger.error(`[${err.contex}] ${err.message}`);
+            res.status(err.statusCode).send({ err: err.message })
         } else {
-            this.logerSevice.error(`${err.message}`)
-            res.status(500).send({err: err.message})      
+            this.logger.error(`${err.message}`)
+            res.status(500).send({ err: err.message })
         }
         next();
     }
